b1d5839e6e79fb70e461a98838be2763279cdf1a
OriginSvin <36486850+OriginSvin@users.noreply.github.com>
2021-03-18 17:23:54 +0300

    X300

--- build/Windows/MorseServer/etc/static/js/particles.js
+++ build/Windows/MorseServer/etc/static/js/particles.js
@@ -1,18 +1,46 @@
 function validate_signup()
 {
 	var counter = 0;
-	if (document.getElementById("pas").value == "")
+	if (document.getElementById("pas").value == "" || document.getElementById("pas2").value == "")
 	{
 		counter++;
-		//document.getElementById("passErrSign").css('display', 'block');
+		document.getElementById("err").innerHTML = 'Пароль необходимо ввести дважды';
 	}
 	if (document.getElementById("pas2").value == "" || document.getElementById("pas2").value !== document.getElementById("pas").value)
 	{
 		counter++;
-		//document.getElementById("rPassErrSign").css('display', 'block');
-		document.getElementById("pas2").value = '';
+		document.getElementById("err").innerHTML = 'Пароли должны совпадать';
+	}
+	if (document.getElementById("log").value == "")
+	{
+		counter++;
+		document.getElementById("err").innerHTML = 'Необходимо ввести логин';
 	}
 	return counter ? false : true;
 }
-
-
+function validate_signin()
+{
+	var counter = 0;
+	if (document.getElementById("log").value == "" )
+	{
+		counter++;
+		document.getElementById("err").innerHTML = 'Необходимо ввести логин';
+	}
+	if (document.getElementById("pas").value == "")
+	{
+		counter++;
+		document.getElementById("err").innerHTML = 'Необходимо ввести пароль';
+	}
+	return counter ? false : true;
+}
+function validate_word()
+{
+	var counter = 0;
+	if (document.getElementById("code").value == "" )
+	{
+		counter++;
+		document.getElementById("err").innerHTML = 'Необходимо ввести ответ';
+	}
+	
+	return counter ? false : true;
+}
